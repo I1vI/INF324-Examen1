cdeaea2eb54f47f496c4a5f4aa5c7e4e27a3d72a
I1vI <141283626+I1vI@users.noreply.github.com>
2024-10-04 13:25:25 -0400

    Add files via upload

---
+++ Ejercicio8/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola Mundo en PHP</title>
+    <!-- llamado a mi archivo bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
+        <div class="text-center">
+            <h1 class="display-1 fw-bold text-primary">Hola Mundo</h1>
+        </div>
+    </div>
+    <!-- llamado a mi archivo bootstrap JS -->
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
